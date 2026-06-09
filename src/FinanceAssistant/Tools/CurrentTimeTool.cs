ce38481200a7234d172ee46de53b012e3c4f2233
Volkmar Rigo <volkmar@rigo.bz>
2026-06-09 09:47:13 +0200

    Tried to fix the day from context problem

--- src/FinanceAssistant/Tools/CurrentTimeTool.cs
+++ src/FinanceAssistant/Tools/CurrentTimeTool.cs
@@ -4,6 +4,8 @@ namespace FinanceAssistant.Tools;
 
 public class CurrentTimeTool
 {
-    [Description("Returns the current time in the ISO 8601 format. Use this when the user asks about the current date or time")]
+    [Description("Returns the current time in the ISO 8601 format. Always call this tool whenever the user " +
+                 "asks about the current date or time, even if you already have a date in your context. " +
+                 "Never rely on your training data or context for the current date or time.")]
     public string GetCurrentTime() =>  DateTimeOffset.Now.ToString("O");
 }
